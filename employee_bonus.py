0fd61474ad740e797257120eff1f79ffe7d0a319
LiamSpen <liam_spencer1@baylor.edu>
2023-01-30 23:36:50 -0600

    Fixed error

--- employee_bonus.py
+++ employee_bonus.py
@@ -2,6 +2,12 @@ import csv
 
 with open("EmployeePay.csv", 'r') as Employee_Pay:
     Employee_Pay = csv.reader(Employee_Pay)
+    next(Employee_Pay)
+
+    print("ID    First Name      Last Name       Salary          Bonus           Total Pay")
+    print("-------------------------------------------------------------------------------")
 
     for line in Employee_Pay:
-        print(line)
+        totalPay = float(line[3]) * (1+float(line[4]))
+        print(
+            f"{line[0]:5s} {line[1]:15s} {line[2]:15s} {line[3]:15s} {line[4]:15s} ${round(totalPay,2)}")
